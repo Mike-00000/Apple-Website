d7e8c36e5ccd96849e547ef05cb0d73eb80556e2
Mike <mt.mail.mt@gmail.com>
2024-03-19 15:40:15 +0200

    Commiting some advancements

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Html, PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import IPhone from "./IPhone"
 
-const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
-    <View></View>
-  )
-}
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : ''`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
 
-export default ModelView
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <IPhone />
+      </Suspense>
+    </View>
+  );
+};
+
+export default ModelView;
