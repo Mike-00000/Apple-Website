f4906eec5fce211695d339a7b29cddfa6d8a88bb
Mike <mt.mail.mt@gmail.com>
2024-03-19 14:21:42 +0200

    Working on 3D section

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
+  return (
+    <View></View>
+  )
+}
+
+export default ModelView
